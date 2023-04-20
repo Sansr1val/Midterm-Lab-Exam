f397ef3f7d5fc6fc7787e83e285cb65bd4001f5b
John Jomar <dimaunahanjohnjomar@gmail.com>
2023-04-21 04:33:15 +0800

    Add Main method and addNode method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	static PersistentLinkedList list = new PersistentLinkedList();
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("\n\n-----Menu-----");
+		System.out.println("[1] Add Node");
+		System.out.println("[2] Delete Node");
+		System.out.println("[3] Display");
+		System.out.println("[4] Change Value");
+		System.out.println("[5] Node History");
+		System.out.println("[6] List History");
+		System.out.println("[7] Exit");
+		System.out.print("Enter Command: ");
+		int command = Integer.parseInt(reader.readLine());	
+		switch (command) {
+		case 1:
+			break;
 
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+
+		case 5:
+			break;
+
+		case 6:
+			break;
+
+		case 7:
+			break;
+
+		default:
+			System.out.println("Invalid command. Please try again.");
+			break;
+		}
+
+	}
+	
+	public void addNode() throws NumberFormatException, IOException {
+		System.out.print("Enter new node's value: ");
+		int value = Integer.parseInt(reader.readLine());
+		list.add(value);
+		System.out.println("Node was successfully added!");
 	}
 
 }
