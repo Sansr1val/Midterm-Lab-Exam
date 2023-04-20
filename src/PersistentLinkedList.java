af85c7e142ac8cbdcff002b1b5c9c24687e3f565
John Jomar <dimaunahanjohnjomar@gmail.com>
2023-04-21 03:46:16 +0800

    Added add function

--- src/PersistentLinkedList.java
+++ src/PersistentLinkedList.java
@@ -1,9 +1,43 @@
 
 public class PersistentLinkedList {
 	
-	private Node head;
-	private Node tail;
+	private PersistentNode head;
+	private PersistentNode tail;
+	private IntegerArray deletedNodePosition;
+	private PersistentNodeArray deletedNodeArray;
 	
+	public PersistentLinkedList() {
+		head = null;
+		tail = null;
+		deletedNodePosition = new IntegerArray();
+		deletedNodeArray = new PersistentNodeArray();
+	}
+	
+	public void add(int data) {
+		PersistentNode node = new PersistentNode(data);
+		if(head == null || tail == null) {
+			head = node;
+			tail = node;
+		}else {
+			tail.setNext(node);
+			node.setPrevious(tail);
+			tail = node;
+		}
+	}
+	
+	public void delete(int position) {
+		
+	}
+	
+	public String display() {
+		return "";
+	}
+	public String displayNodeHistory(int position) {
+		return "";
+	}
+	public String displayListHistory() {
+		return "";
+	}
 	
 	
 
